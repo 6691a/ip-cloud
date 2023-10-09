e1b8e24628533e72bea6df52529f2cdfa507a90c
6691a <6691a@naver.com>
2023-10-09 18:17:57 +0900

    email login setup

--- app/views.py
+++ app/views.py
@@ -1,9 +1,10 @@
 from django.views.generic import TemplateView
 
+from utility.form.mixin import LayoutMixin
 from utility.template.layout import TemplateLayout
 
 
-class IndexView(TemplateView):
+class IndexView(LayoutMixin, TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
